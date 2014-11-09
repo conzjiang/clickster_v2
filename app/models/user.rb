e617126ce225a33259e696537ab2c0d621ace7f5
Constance Jiang <conzjiang@gmail.com>
2014-11-09 16:43:05 -0500

    Users can sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ActiveRecord::Base
+  include BCrypt
+  attr_reader :password
+
   validates :email, :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end

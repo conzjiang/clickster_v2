3b015df0d8b41d8ddae0b28979cfa015cb14cbc3
Constance Jiang <conzjiang@gmail.com>
2014-11-09 17:13:36 -0500

    Add tests to sign-in-related User methods.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,27 @@ describe User do
     it { should validate_uniqueness_of(attribute) }
   end
 
-  it "should generate session token upon initialization" do
-    expect(user.session_token).not_to be_nil
+  describe "#ensure_session_token" do
+    it "should generate session token upon initialization" do
+      expect(user.session_token).not_to be_nil
+    end
+  end
+
+  describe "::find_by_credentials" do
+    it "can search by username" do
+      found_user = User.find_by_credentials(user.username, user.password)
+      expect(found_user).to eq(user)
+    end
+
+    it "can search by email" do
+      found_user = User.find_by_credentials(user.email, user.password)
+      expect(found_user).to eq(user)
+    end
+
+    it "returns nil if no matches" do
+      found_user = User.find_by_credentials("qqqqqq", "qqqqqq")
+      expect(found_user).to be_nil
+    end
   end
 
   describe "#password" do
@@ -19,8 +38,15 @@ describe User do
       expect(build(:user, password: nil)).to be_valid
     end
 
-    it "should automatically generate password_digest" do
+    it "should automatically generate password digest" do
       expect(user.password_digest).not_to be_nil
+    end
+  end
+
+  describe "#reset_session_token!" do
+    it "should generate a new session token" do
+      original_token = user.session_token
+      expect(user.reset_session_token!).not_to eq(original_token)
     end
   end
 end

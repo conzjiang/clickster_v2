712dfe33f18a1f86dd0726026b5b718d709cec6e
Constance Jiang <conzjiang@gmail.com>
2014-11-12 15:45:59 -0500

    Move hamburger icon and sign-in view to navbar view + style search bar.

--- app/assets/javascripts/views/sidebar.js
+++ app/assets/javascripts/views/sidebar.js
@@ -1,13 +1,24 @@
 Clickster.Views.Sidebar = Backbone.View.extend({
   initialize: function () {
-    this.listenTo(Clickster.currentUser, 'sync', this.render);
   },
 
   template: JST['sidebar'],
 
+  events: {
+    'click input[name=query]': 'expandInput',
+    'blur input[name=query]': 'revertInput'
+  },
+
+  expandInput: function () {
+    $(event.target).addClass('open');
+  },
+
+  revertInput: function () {
+    $(event.target).removeClass('open');
+  },
+
   render: function () {
-    var content = this.template({ signedIn: !!Clickster.currentUser.id });
-    this.$el.html(content);
+    this.$el.html(this.template());
     return this;
   }
 });

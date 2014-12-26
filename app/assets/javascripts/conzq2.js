6cf467de8e053688f3fed29e73d58c80ae747ec2
Constance Jiang <conzjiang@gmail.com>
2014-12-26 14:41:23 -0600

    Move constant declarations to html root page.

--- app/assets/javascripts/conzq2.js
+++ app/assets/javascripts/conzq2.js
@@ -8,12 +8,6 @@ window.Clickster = {
     Clickster.tvShows = new Clickster.Collections.TvShows();
     Clickster.users = new Clickster.Collections.Users();
 
-    Clickster.formAuth = $('meta[name=csrf-token]').attr("content");
-    Clickster.DECADES = JSON.parse($('#decades').html());
-    Clickster.GENRES = JSON.parse($('#genres').html());
-    Clickster.TV_STATUSES = JSON.parse($('#statuses').html());
-    Clickster.LIST_STATUSES = JSON.parse($('#list-statuses').html());
-
     Clickster.filepickerOptions = {
       mimetype: "image/*",
       services: ["COMPUTER", "URL"]

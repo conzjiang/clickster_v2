db843f3083170071842d1cf80ca40fd1a5f09f19
Constance Jiang <conzjiang@gmail.com>
2014-12-26 15:05:41 -0600

    Finish BB user model specs.

--- spec/javascripts/models/user_spec.js
+++ spec/javascripts/models/user_spec.js
@@ -1,15 +1,71 @@
 //= require application
 
 describe("User model", function () {
+  var userModel, currentUser, user, response;
+
+  beforeEach(function () {
+    userModel = Clickster.Models.User;
+    currentUser = Clickster.currentUser = { id: 1 };
+    user = new userModel({ id: 1, username: "conz" });
+    response = {
+      watchlists: [
+        { id: 1, status: "Watching" },
+        { id: 2, status: "Completed" },
+        { id: 3, status: "Watching" }
+      ],
+      favorites: [{ id: 2 }]
+    };
+
+    user.parse(response);
+  });
+
   it("initializes with a username", function () {
-    Clickster.currentUser = {};
-    var user = new Clickster.Models.User({ username: "conz" });
     expect(user.get("username")).to.equal("conz");
   });
 
   it("correctly assigns itself as the current user", function () {
-    Clickster.currentUser = { id: 1 };
-    var user = new Clickster.Models.User({ username: "conz", id: 1 });
+    var otherUser = new userModel({ id: 2, username: "other" });
+
     expect(user.isCurrentUser).to.be.true;
+    expect(otherUser.isCurrentUser).to.be.false;
+  });
+
+  it("correct url", function () {
+    expect(user.url()).to.equal("/api/users/conz");
+  });
+
+  describe("#parse", function () {
+    it("sets the correct collections when parsing", function () {
+      expect(user.watchlists().length).to.equal(3);
+      expect(user.favorites().length).to.equal(1);
+    });
+
+    it("deletes the parsed fields from the response", function () {
+      expect(response).to.be.empty;
+    });
+  });
+
+  describe("#watchlists & #favorites", function () {
+    it("are instances of Lists", function () {
+      [user.watchlists(), user.favorites()].forEach(function (collection) {
+        expect(collection instanceof Clickster.Collections.Lists).to.be.true;
+      });
+    });
+
+    it("always return the same collection", function () {
+      expect(user.watchlists()).to.equal(user.watchlists());
+      expect(user.favorites()).to.equal(user.favorites());
+    });
+
+    it("filters watchlists based on status", function () {
+      var completed = user.watchlists("Completed");
+      expect(completed.length).to.equal(1);
+      expect(completed[0].id).to.equal(2);
+    });
+
+    it("have respective count functions", function () {
+      expect(user.watchNum()).to.equal(2);
+      expect(user.favoriteCount()).to.equal(1);
+    });
   });
 });

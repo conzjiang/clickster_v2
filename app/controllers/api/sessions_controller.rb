0c64ba7ff8d93ed708e1d4d82d43e747f5437176
Constance Jiang <conzjiang@gmail.com>
2015-08-20 17:26:29 -0400

    Add facebook action to Api::SessionsController and render jbuilder.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,21 +1,27 @@
 class Api::SessionsController < ApplicationController
   def create
-    user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:identifier],
       params[:user][:password]
     )
 
-    if user
-      sign_in!(user)
-      render json: user, status: 200
+    if @user
+      sign_in!(@user)
+      render 'users/show'
     else
       render json: ['Incorrect username/password'], status: 422
     end
   end
 
   def demo
-    demo_user = CreateDemoUser.go!
-    sign_in!(demo_user)
-    render json: demo_user, status: 200
+    @user = CreateDemoUser.go!
+    sign_in!(@user)
+    render 'users/show'
+  end
+
+  def facebook
+    @user = User.find_or_create_by_omniauth_params(params[:facebook])
+    sign_in!(@user)
+    render 'users/show'
   end
 end

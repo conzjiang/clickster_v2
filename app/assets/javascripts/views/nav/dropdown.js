a0c05f4e1ac9ea8733e174e706b8e99a93c3818a
Constance Jiang <conzjiang@gmail.com>
2015-08-25 14:49:09 -0400

    Set up authenticity token in Dropdown view.

--- app/assets/javascripts/views/nav/dropdown.js
+++ app/assets/javascripts/views/nav/dropdown.js
@@ -1,7 +1,7 @@
 Clickster.Views.DropdownView = Backbone.View.extend({
-  className: "content",
-
   template: JST["nav/dropdownMenu"],
+
+  className: "content",
 
   events: {
     "touchstart .nav-link": "mobileTouch",
@@ -15,10 +15,17 @@ Clickster.Views.DropdownView = Backbone.View.extend({
 
   render: function () {
     var isAdmin = Clickster.currentUser.get("is_admin");
-    var content = this.template({ isAdmin: isAdmin });
+    var content = this.template({
+      isAdmin: isAdmin
+    });
 
     this.$el.html(content);
     if (isAdmin) this.$(".dropdown").addClass("admin");
+
     return this;
+  },
+
+  onRender: function () {
+    $.rails.refreshCSRFTokens();
   }
 });

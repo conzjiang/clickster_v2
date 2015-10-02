e9d11d602ffb9dd53203b6cbd8c6b8bc53c18226
Constance Jiang <conzjiang@gmail.com>
2015-10-02 16:51:59 -0400

    Redirect to #facebook fragment if new user logs in through FB.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,13 +27,29 @@ class SessionsController < ApplicationController
   end
 
   def facebook
-    user = User.find_or_create_by_omniauth_params(request.env['omniauth.auth'])
-    sign_in!(user)
-    flash[:notice] = "Successfully signed in!"
-    redirect_to root_url
+    user = User.find_by_omniauth_params(omniauth_hash)
+
+    if user
+      sign_in!(user)
+      redirect_to root_url
+    else
+      user = User.create_from_omniauth_params!(omniauth_hash)
+      sign_in!(user)
+      redirect_to "#{root_url}#facebook"
+    end
   end
 
   private
+  def omniauth_hash
+    req_hash = request.env['omniauth.auth']
+
+    {
+      uid: req_hash[:uid],
+      email: req_hash[:info][:email],
+      name: req_hash[:info][:name]
+    }
+  end
+
   def user_params
     params[:user][:username] = params[:user][:identifier]
     params[:user].delete(:identifier)
